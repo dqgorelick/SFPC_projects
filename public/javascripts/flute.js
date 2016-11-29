73c7eedec083f068adebda5c5fc135d6d2fde3bf
Daniel Gorelick <dqgorelick@gmail.com>
2016-11-28 19:29:51 -0500

    OOP javascript kinda

--- public/javascripts/flute.js
+++ public/javascripts/flute.js
@@ -1,23 +1,95 @@
-var CANVAS_TOP = 150; // get this from the css file from .note
+var CANVAS_TOP = 225; // get this from the css file from .note
 
 var TEST_SONG = [7,11,9,11,7,11,9,11,7,12,10,12,7,12,10,12];
 
 var MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11];
 
 var BASE_NOTE = 48;
+var SONG_RATE = 1000;
+
 var NOTE_DURATION = 200; // 0.2 second delay set in SuperCollider
+
 var STEPS = 16;
 
-var rotationAngle = 0;
 var notes = [];
 
-var player = {
-  leftHand: null,
-  rightHand: null,
-};
+Player = function (sendNote) {
+  this.leftHand = null;
+  this.rightHand = null;
+  this.meter = null;
+  this.sendNote = sendNote;
+  this.songIndex = 0;
+  this.noteCount = 0;
+  console.log('this.sendNote',this.sendNote);
+}
 
-// used for setTimeout
-var meter;
+Player.prototype.start = function() {
+  console.log('this',this);
+  var that = this;
+  function nextNote() {
+    // TODO subtract one from each note in TEST_SONG and don't hardcode the octave shift
+    var midiNote = 12 + BASE_NOTE + MAJOR_SCALE[(TEST_SONG[that.songIndex]) % 7];
+
+    // get actual note mappings
+    var current = TEST_SONG[that.songIndex];
+
+    var nextIndex = (that.songIndex + 1) % TEST_SONG.length;
+    var next = TEST_SONG[nextIndex];
+
+    // if neither hand is setup
+    if (!that.rightHand && !that.leftHand) {
+      if (current < next) {
+        that.leftHand = notes[current];
+        that.rightHand = notes[next];
+      } else {
+        that.leftHand = notes[next];
+        that.rightHand = notes[current];
+      }
+    } else {
+      // reassign notes
+      if (next > current) {
+        that.leftHand = notes[current];
+        that.rightHand = notes[next];
+      } else  {
+        that.leftHand = notes[next];
+        that.rightHand = notes[current];
+      }
+    }
+
+    that.render();
+    // play note
+    console.log('midiNote',midiNote);
+    that.sendNote(midiNote);
+    animateNote(midiNote);
+
+    // recursive call at SONG_RATE
+    that.meter = setTimeout(nextNote, SONG_RATE);
+
+    that.noteCount++;
+    that.songIndex++;
+    if (that.songIndex === TEST_SONG.length) that.songIndex = 0;
+  }
+
+  nextNote();
+}
+
+Player.prototype.render = function() {
+  $('#player span').css('-webkit-transform', 'rotate(' + (this.noteCount + 1)* 180 + 'deg)');
+  $('#player span').css('transform:','rotate(' + (this.noteCount + 1) * 180 + 'deg)');
+
+  var diameter = this.rightHand.center - this.leftHand.center;
+  var top = CANVAS_TOP + this.leftHand.width/2 - diameter/2;
+  var left = this.leftHand.center;
+  $('#player span').css('top', top);
+  $('#player span').css('left', left);
+  $('#player span').css('height', diameter);
+  $('#player span').css('width', diameter);
+}
+
+Player.prototype.stop = function() {
+  clearTimeout(this.meter);
+}
+
 
 // create the note objects
 function createNotes(baseNote, cb) {
@@ -59,57 +131,25 @@ function renderNotes() {
     })());
 }
 
-function rotatePlayer() {
-  rotationAngle += 180;
-  $('#player span').css('-webkit-transform', 'rotate(' + rotationAngle + 'deg)');
-  // $('#player span').css('-moz-transform', 'rotate(' + rotationAngle + 'deg)');
-  // $('#player span').css('-o-transform', 'rotate(' + rotationAngle + 'deg)');
-  // $('#player span').css('-ms-transform', 'rotate(' + rotationAngle + 'deg)');
-  $('#player span').css('transform:','rotate(' + rotationAngle + 'deg)');
+function initializeSettings() {
+  $('.note').css('top', CANVAS_TOP);
+  $('#player span').css('-webkit-transition', 'all ' + SONG_RATE + 'ms linear');
+  $('#player span').css('transition', 'all ' + SONG_RATE + 'ms linear');
 }
 
-function updatePlayer(songIndex, cb) {
-  // get next song index =
-  var nextIndex = (songIndex + 1) % TEST_SONG.length;
 
-  // get actual note mappings
-  var current = TEST_SONG[songIndex];
-  var next = TEST_SONG[nextIndex];
-
-  console.log('current',current);
-  console.log('next',next);
-  // if neither hand is setup
-  if (!player.rightHand && !player.leftHand) {
-    player.rightHand = notes[current];
-    player.leftHand = notes[current];
-  }
-
-  // decide which hand to move
-  // defaults to move right hand for higher notes
-  // moves left hand for lower notes
-  if (next > current) {
-    player.rightHand = notes[next];
-  } else if (next < current) {
-    player.leftHand = notes[next];
-  }
-
-  console.log('player',player);
-  cb();
-}
-
-function renderPlayer() {
-    var diameter = player.rightHand.center - player.leftHand.center;
-    var top = CANVAS_TOP + player.leftHand.width/2 - diameter/2;
-    var left = player.leftHand.center;
-    $('#player span').css('top', top);
-    $('#player span').css('left', left);
-    $('#player span').css('height', diameter);
-    $('#player span').css('width', diameter);
+function animateNote(note) {
+  // note transition
+  var activeNote = $('#'+note);
+  activeNote.addClass('active');
+  setTimeout(function() {
+    activeNote.removeClass('active');
+  }, NOTE_DURATION);
 }
 
 $(document).ready(function() {
   /*
-      Incoming data on socket
+      SOCKET OPERATIONS
    */
   var socket = new WebSocket('ws://0.0.0.0:8081/');
 
@@ -117,57 +157,30 @@ $(document).ready(function() {
       var result = JSON.parse(evt.data);
   };
 
-  // render notes
+  function sendNote (note) {
+    socket.send(note);
+  }
+
+  /*
+    INITIALIZE
+   */
   createNotes(BASE_NOTE, renderNotes);
+  initializeSettings();
+
+  // create sequencer
+  var mozart = new Player(sendNote);
 
   $('#start').on('click', function() {
-    console.log('start');
-    playSong();
+    mozart.start();
   });
 
   $('#stop').on('click', function() {
-    console.log('stop');
-    clearTimeout(meter);
+    mozart.stop();
   });
 
-  $('#rotate').on('click', function() {
-    console.log('rotate');
-    // $('#player span').addClass('rotate');
-    rotatePlayer()
-  });
-
-  $('#rotate2').on('click', function() {
-    console.log('rotate2');
-    $('#player span').removeClass('rotate');
-  });
-
-  function playSong() {
-    var songIndex = 0;
-    function nextNote() {
-      // TODO subtract one from each note in TEST_SONG and don't hardcode the octave shift
-      var midiNote = 12 + BASE_NOTE + MAJOR_SCALE[(TEST_SONG[songIndex]) % 7];
-      sendNote(midiNote);
-      songIndex++;
-
-      // note transition
-      var activeNote = $('#'+midiNote);
-      activeNote.addClass('active');
-      setTimeout(function() {
-        activeNote.removeClass('active');
-      }, NOTE_DURATION);
-
-      // player logic
-      rotatePlayer();
-      setTimeout(updatePlayer(songIndex, renderPlayer), 250);
-
-      if (songIndex === TEST_SONG.length) songIndex = 0;
-      meter = setTimeout(nextNote, 250);
-    }
-
-    nextNote();
-  }
-
-  // event listeners for the user
+  /*
+    USER INPUT
+   */
   $('.note').on('click', function(evt) {
       var note = $(this).attr("id")
       sendNote(note);
@@ -177,9 +190,4 @@ $(document).ready(function() {
       var note = $(this).attr("id")
       sendNote(note);
   });
-
-  function sendNote (note) {
-    console.log('note',note);
-    socket.send(note);
-  }
-})
+});
